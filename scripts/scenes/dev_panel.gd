4dabc87beefc6d675bc66d410b00370962c20a15
Evan McPheron <evaninja2@gmail.com>
2023-06-23 21:47:55 -0500

    Updated Dev Panel

--- scripts/scenes/dev_panel.gd
+++ scripts/scenes/dev_panel.gd
@@ -4,11 +4,15 @@ extends CanvasLayer
 }
 @export var showing := false
 
+func set_entry(key, value) -> void:
+	self.entries[key] = value
 
 func _process(_delta):
-	var buf := "";
 	if showing:
-		$"container/Label".text = str(entries)
+		var buf = "\n"
+		for key in self.entries.keys():
+			buf += "  " + str(key) + ": " + str(self.entries[key]) + "\n"
+		$"container/Label".text = buf
 
 func cart_to_polar(point: Vector2):
-	
+	pass
